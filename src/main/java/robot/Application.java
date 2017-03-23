9358b41de9e1c39d5e4e3a8a8038a22504ae3858
viniciuspalma <pockvini@gmail.com>
2017-03-23 01:26:04 -0300

    prepare a rest controller for mars module requests

--- src/main/java/robot/Application.java
+++ src/main/java/robot/Application.java
@@ -1,7 +1,12 @@
 package robot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
   public static void main(String[] args) {
-    System.out.println("olar");
+    SpringApplication.run(Application.class, args);
   }
 }
