f2decbea516bd1be1030819eb970188a5cf431cd
viniciuspalma <pockvini@gmail.com>
2017-03-24 03:58:28 -0300

    configure the initial states of Robot and Map in controller

--- src/main/java/robot/mars/support/MarsController.java
+++ src/main/java/robot/mars/support/MarsController.java
@@ -1,14 +1,30 @@
 package robot.mars.support;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import robot.mars.support.Robot;
+import robot.mars.support.CommandsParser;
+import robot.mars.support.ExecuteCommands;
+import robot.mars.support.Robot;
+import robot.mars.support.MarsMap;
 
 @RestController
+@RequestMapping("/rest/mars")
 public class MarsController {
 
-  @RequestMapping("rest/mars/{commands}")
-  public String greeting(@PathVariable(value="commands") String commands) {
-    return commands;
+  @RequestMapping(value = "/{commands}", method = POST)
+  public String walk(@PathVariable(value="commands") String commands) {
+    MarsMap marsMap = new MarsMap(5, 5);
+    Robot robot = new Robot(0, 0, 'N', marsMap);
+
+    List<Character> parsedCommands = new CommandsParser(commands).parsedCommands();
+    new ExecuteCommands().iterateCommands(parsedCommands, robot);
+
+    return robot.toString();
   }
 }
